b102e4498b28e5aefa45515e9f27d662bffe7bd6
William <gcphost@gmail.com>
2014-04-19 21:21:40 -0500

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => '127.0.0.1',
 			'database'  => 'laravelbootstrapstarter',
 			'username'  => 'root',
-			'password'  => 'tical',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
