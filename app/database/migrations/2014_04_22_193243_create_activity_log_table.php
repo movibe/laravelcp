40f4dc43a9382ddd1f1329a11822e900387648b4
william bowman <gcphost@gmail.com>
2014-04-24 18:42:32 -0500

    activity long update
    updated details to be longtext

--- app/database/migrations/2014_04_22_193243_create_activity_log_table.php
+++ app/database/migrations/2014_04_22_193243_create_activity_log_table.php
@@ -18,7 +18,7 @@ class CreateActivitylogTable extends Migration {
             $table->string('content_type', 72);
             $table->string('action', 32);
             $table->string('description', 255);
-            $table->string('details', 255);
+            $table->longText('details');
             $table->boolean('developer');
             $table->string('ip_address', 64);
             $table->string('user_agent', 255);
