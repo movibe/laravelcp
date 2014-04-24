d4ca7137e0dea702b0f82251b5d48bb85b7cf53b
william bowman <gcphost@gmail.com>
2014-04-23 22:54:11 -0500

    updated migration files
    includes new slug columns since its now a really basic cms
    added new profile columns for oauth

---
+++ app/database/migrations/2014_04_21_193243_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function($table) {
+            $table->increments('id');
+            $table->string('email', 255);
+            $table->string('password', 255);
+            $table->string('confirmation_code', 255);
+            $table->boolean('confirmed');
+            $table->timestamp('created_at')->default("0000-00-00 00:00:00");
+            $table->timestamp('updated_at')->default("0000-00-00 00:00:00");
+            $table->timestamp('last_activity')->default("0000-00-00 00:00:00");
+            $table->timestamp('last_login')->default("0000-00-00 00:00:00");
+            $table->string('displayname', 256);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+
+}
---
+++ app/database/migrations/2014_04_21_203243_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function($table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('title', 255);
+            $table->string('slug', 255);
+            $table->text('content');
+            $table->string('meta_title', 255);
+            $table->string('meta_description', 255);
+            $table->string('meta_keywords', 255);
+            $table->timestamp('created_at')->default("0000-00-00 00:00:00");
+            $table->timestamp('updated_at')->default("0000-00-00 00:00:00");
+ 			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+       });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+
+}
