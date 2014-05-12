3d1f9ac8e94c99b2eaa10f365c8da55e7a68fcc5
william bowman <gcphost@gmail.com>
2014-05-12 12:21:23 -0500

    Switched off the cdn for js/css
    User complaint of speed issues, cnd slowed it down a little, not
    anymore.
    Still looking for other causes of slow speed.

--- app/views/default/admin/css.blade.php
+++ app/views/default/admin/css.blade.php
@@ -1,13 +1,13 @@
-<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="{{{ asset('assets/css/bootstrap.min.css') }}}" />
 @if(Setting::get('site.bootswatch'))
 <link rel="stylesheet" type="text/css" href="//bootswatch.com/{{{ Setting::get('site.bootswatch') }}}/bootstrap.min.css" />
 @else
 <link rel="stylesheet" type="text/css" href="//bootswatch.com/assets/css/bootswatch.min.css" />
 @endif
-<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-<link rel="stylesheet" href="//cdn.jsdelivr.net/select2/3.4.8/select2.css">
+<link href="{{{ asset('assets/css/font-awesome.css') }}}" rel="stylesheet">
+<link rel="stylesheet" href="{{{ asset('assets/css/select2.css') }}}">
 <link rel="stylesheet" href="{{{ asset('assets/css/style.css') }}}"/>
 
-<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/3.0.0/css/bootstrap-datetimepicker.min.css"/>
+<link rel="stylesheet" href="{{{ asset('assets/css/bootstrap-datetimepicker.min.css') }}}"/>
 
 @yield('styles')
--- app/views/default/admin/js.blade.php
+++ app/views/default/admin/js.blade.php
@@ -1,5 +1,5 @@
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	<script src="{{{ asset('assets/js/jquery.min.js') }}}"></script>
+	<script src="{{{ asset('assets/js/bootstrap.min.js') }}}"></script>
 	<script src="{{{ asset('assets/js/bootstrap-wysiwyg-start.js') }}}"></script>
 	<script src="{{{ URL::to('translation.js') }}}"></script>
 	<script src="{{{ asset('assets/js/jquery.poller.js') }}}"></script>
@@ -8,18 +8,13 @@
 		$.fn.poller({'url':'{{{ URL::to("admin/polling") }}}'});
 	</script>
 
-	<script src="//cdn.jsdelivr.net/select2/3.4.8/select2.min.js"></script>
-
+	<script src="{{{ asset('assets/js/select2.min.js') }}}"></script>
 	<script src="{{{ asset('assets/js/site.js') }}}"></script>
-	
-	<script src="//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/jquery.dataTables.min.js"></script>
+	<script src="{{{ asset('assets/js/jquery.dataTables.min.js') }}}"></script>
 	<script src="{{{ asset('assets/js/datatables.fnReloadAjax.js') }}}"></script>
 	<script src="{{{ asset('assets/js/datatables-bootstrap.js') }}}"></script>
 	<script src="{{{ asset('assets/js/datatables.js') }}}"></script>
-
-	<script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-growl/1.0.0/jquery.bootstrap-growl.min.js"></script>
-
-	<script src="//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.2.0/bootbox.min.js"></script>
-
-	<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.6.0/moment.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/3.0.0/js/bootstrap-datetimepicker.min.js"></script>
+	<script src="{{{ asset('assets/js/jquery.bootstrap-growl.min.js') }}}"></script>
+	<script src="{{{ asset('assets/js/bootbox.min.js') }}}"></script>
+	<script src="{{{ asset('assets/js/moment.min.js') }}}"></script>
+	<script src="{{{ asset('assets/js/bootstrap-datetimepicker.min.js') }}}"></script>
